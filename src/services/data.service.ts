fa2c0723c6cbd5489c474eb519652d25aea18750
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2024-05-21 22:55:31 +0100

    Updated some tech stack icons

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -5,3 +5,12 @@ export const getLandingData = async (): Promise<LandingPageData> => {
   const data: LandingPageData = landingData;
   return data;
 };
+
+
+import type { WebPageData } from "@/config/web.interface";
+import webData from "@/data/web.json";
+
+export const getWebData = async (): Promise<WebPageData> => {
+  const data: WebPageData = webData;
+  return data;
+};
