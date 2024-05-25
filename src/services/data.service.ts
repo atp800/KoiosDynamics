210d4ccda28868f81981b0812347afdadbbba83d
Andreas Panayiotou (BA/BSc Liberal Arts + Sciences) <atp800@student.bham.ac.uk>
2024-05-25 13:28:46 +0100

    Added resources for other pages (ai, analytics, coding)

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,3 +1,4 @@
+// Homepage
 import type { LandingPageData } from "@/config/landing.interface";
 import landingData from "@/data/landing.json";
 
@@ -7,6 +8,7 @@ export const getLandingData = async (): Promise<LandingPageData> => {
 };
 
 
+// Web Development
 import type { WebPageData } from "@/config/web.interface";
 import webData from "@/data/web.json";
 
@@ -14,3 +16,34 @@ export const getWebData = async (): Promise<WebPageData> => {
   const data: WebPageData = webData;
   return data;
 };
+
+
+// Analytics and Dashboards
+import type { AnalyticsPageData } from "@/config/analytics.interface";
+import analyticsData from "@/data/analytics.json";
+
+export const getAnalyticsData = async (): Promise<AnalyticsPageData> => {
+  const data: AnalyticsPageData = analyticsData;
+  return data;
+};
+
+
+// Data science and AI modelling
+import type { AIPageData } from "@/config/ai.interface";
+import aiData from "@/data/ai.json";
+
+export const getAIData = async (): Promise<AIPageData> => {
+  const data: AIPageData = aiData;
+  return data;
+};
+
+
+// Programming
+import type { CodingPageData } from "@/config/coding.interface";
+import codingData from "@/data/coding.json";
+
+export const getCodingData = async (): Promise<CodingPageData> => {
+  const data: CodingPageData = codingData;
+  return data;
+};
+
